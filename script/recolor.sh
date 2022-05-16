e241fce6fff0abf438adfffc50b4cad67f34af41
Pascal Jäger <pascal.jaeger@leimstift.de>
2022-05-16 23:07:40 +0200

    Updated recolor.sh script to use user prompt

--- script/recolor.sh
+++ script/recolor.sh
@@ -17,29 +17,38 @@ CYAN='\033[;36m'
 RESET='\033[0m'
 
 # Check for arguments
-if [[ $# -ne 8 ]]; then
+if [[ $# -ne 1 ]]; then
   # Print error to stderr
-  echo -e "${RED}[!] $# arguments passed, 8 required${RESET}" >&2
+  echo -e "${RED}[!] $# arguments passed, 1 required${RESET}" >&2
   # Print usage
-  echo -e "${CYAN}[?] usage: ./recolor.sh [file] [output] [gradient_color_top] [gradient_color_bottom] [letter_shadow] [sphere_shadow] [background] [light_background]${RESET}"
-  echo -e "${CYAN}[?] Colors should be in HEX format: 434C5E, f13${RESET}"
+  echo -e "${CYAN}[?] usage: ./recolor.sh [output filename]${RESET}"
+  echo -e "${CYAN}[?] Colors must be in HEX format: 434C5E, f13${RESET}"
   exit 2
 fi
 
 # Recolor
-cp "$1" "$2" # Copy output file
+# get the colors from the user
+read -p "input the top gradient color of the text. Hex format, e.g. 434c5e: " topgradient
+read -p "input the bottom gradient color of the text: " bottomgradient
+read -p "input the upper outlines color of the doom text: " doomoutlines
+read -p "input the color of the shadows: " shadow
+read -p "input the background color: " background
+read -p "input the color of the dot in the middle that the background gradients to: " middledot
 
-sed -i "s/5e81ac/$3/g" "$2" # Top gradient color of the text
-sed -i "s/b48ead/$4/g" "$2" # Bottom gradient color of the text
-sed -i "s/4c566a/$5/g" "$2" # The upper outlines of the 'DOOM' text
-sed -i "s/3b4252/$6/g" "$2" # The shadow
-sed -i "s/2e3440/$7/g" "$2" # The background
-sed -i "s/434c5e/$8/g" "$2" # Dot in the middle that the background gradients to
+cp "template.svg" "${1}.svg" # Copy output file
+outputfile=${1}".svg"
+
+sed -i "s/fffff9/${topgradient}/g" $outputfile # Top gradient color of the text
+sed -i "s/fffffa/${bottomgradient}/g" $outputfile # Bottom gradient color of the text
+sed -i "s/fffffb/${doomoutlines}/g" $outputfile # The upper outlines of the 'DOOM' text
+sed -i "s/fffffc/${shadow}/g" $outputfile # The shadow
+sed -i "s/fffffd/${background}/g" $outputfile # The background
+sed -i "s/fffffe/${middledot}/g" $outputfile # Dot in the middle that the background gradients to
 
 echo -e "${GREEN}[+] Success!${RESET}"
 
 # If feh command exists, show image
 if [[ $(command -v feh) ]]; then
   echo -e "${CYAN}[?] feh is installed on the system, opening image...${RESET}"
-  feh "$2"
+  feh "$1"
 fi
